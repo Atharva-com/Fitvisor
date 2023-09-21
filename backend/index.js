cab7e251fe3a45b049605b6b007bd5060ef2c588
sujal <sujalforselfuse@gmail.com>
2023-09-21 11:56:16 +0530

    database

---
+++ backend/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+const connectiontomongodb = require('./db');
+const PORT = 8000;
+const cors = require('cors')
+require('dotenv').config();
+
+app.use(cors());
+app.use(express.json());
+app.use('/api/auth', require('./routes/auth'));
+
+connectiontomongodb();
+
+app.get('/', (req, res) => {
+    res.send('Hello fitvisor');
+})
+
+
+app.listen(PORT, () => {
+    console.log("app listening");
+});
+
+
+
